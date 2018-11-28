0e187d86dd9fbf4532560516993fe59aae574d25
linnansved <linn.ansved.6367@student.uu.se>
2018-11-28 13:52:10 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,328 @@
+<!DOCTYPE html>
+<html lang="en-US" prefix="og: http://ogp.me/ns#">
+
+<head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-126323180-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-126323180-1');
+</script>
+
+
+    <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
+
+
+    <!-- fiveiconplaceholder -->
+    <link rel='mask-icon' href='j12mask.svg' color='red'>
+    <meta charset="UTF-8" />
+    <meta name="description" content="J12 Ventures is a Stockholm based Venture Capital firm and Angel network investing early in tech companies." />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <!--
+    Social tags taken from Facebook guidelines:
+    https://developers.facebook.com/docs/sharing/best-practices
+    -->
+    <meta property="og:title" content="J12 Ventures" />
+    <meta property="og:site_name" content="J12 Ventures" />
+    <meta property="og:description" content="J12 Ventures is a Stockholm based Venture Capital firm and Angel network investing early in tech companies." />
+    <meta property="og:url" content="http://j12ventures.com/" />
+
+
+
+    <script src="https://use.typekit.net/air2wsj.js"></script>
+    <script>
+        try {
+            Typekit.load({
+                async: true
+            });
+        } catch (e) {}
+
+    </script>
+
+
+
+    <title>J12 Ventures</title>
+
+    <!-- This site is optimized with the Yoast SEO plugin v4.5 - https://yoast.com/wordpress/plugins/seo/ -->
+    <link rel="canonical" href="http://j12ventures.com/" />
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Home - J12 Ventures" />
+    <meta property="og:url" content="http://j12ventures.com/" />
+    <meta property="og:site_name" content="J12 Ventures" />
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:title" content="Home - J12 Ventures" />
+
+
+    <link rel='dns-prefetch' href='//s.w.org' />
+    <link rel='stylesheet' id='fuzzco_main-css' href='../static/style.min.css' type='text/css' media='all' />
+
+    <!-- <script src="js/modernizr.min.js"></script> -->
+
+
+    <!--
+    <script>
+        eval(mod_pagespeed_klICuhY9Zp);
+    </script>
+
+    <script>
+        eval(mod_pagespeed_Wc_5R6kBlJ);
+    </script>
+    -->
+
+    <script type='text/javascript' src='js/jquery.min.js'></script>
+    <script type='text/javascript' src='js/jquery-migrate.min.js'></script>
+
+
+
+</head>
+
+<body class="home page-template-default page page-id-18" id="page-home">
+    <div class="full-nav">
+        <div class="full-nav-container" style="background-color:red;">
+            <ul id="menu-mobile-menu" class="full-nav-list">
+                <li id="menu-item-156" class="menu-item">
+                    <a class="customROD active-red-menu" href="index.php">Home</a>
+                </li>
+                <li id="menu-item-151" class="menu-item">
+                    <a href="howWeWork.php">How We Work</a>
+                </li>
+                <li id="menu-item-156" class="menu-item">
+                    <a class="customROD" href="people.php">People</a>
+                </li>
+                <li id="menu-item-156" class="menu-item">
+                    <a href="portfolio.php">Portfolio</a>
+                </li>
+                <!--
+                <li id="menu-item-156" class="menu-item">
+                    <a href="insights.php">Insights</a>
+                </li>
+                -->
+                <li id="menu-item-156" class="menu-item">
+                    <a href="contact.php">Contact</a>
+                </li>
+                <li>
+                    <a href="music.php" class="icon" title="J12's playlists">
+                      <img src="images/Spotify-icon-white.png" class="social-icon-menue"
+                           onmouseover="this.src='images/Spotify-icon-purple.png';"
+                           onmouseout="this.src='images/Spotify-icon-white.png';">
+                    </a>
+
+                    <a href="https://www.linkedin.com/company/j12-ventures-ab/" class="icon" target="_blank" title="J12 on Linkedin">
+                      <img src="images/LinkedIn-icon-white.png" class="social-icon-menue"
+                           onmouseover="this.src='images/LinkedIn-icon-purple.png';"
+                           onmouseout="this.src='images/LinkedIn-icon-white.png';">
+                    </a>
+
+                    <a href="https://medium.com/j12ventures" class="icon" target="_blank" title="J12 on Medium">
+                      <img src="images/Medium-icon-white.png" class="social-icon-menue"
+                           onmouseover="this.src='images/Medium-icon-purple.png';"
+                           onmouseout="this.src='images/Medium-icon-white.png';">
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <header id="main-header" style="background-color: #20124dff;">
+        <div class="container">
+            <a href="index.php" class="logo"><img src="images/j12logo.png" alt="J12 Ventures"></a>
+            <nav id="main-menu" role="navigation">
+                <ul id="menu-main-menu" class="no-style horizontal-list">
+                    <li id="menu-item-151" class="menu-item" style>
+                        <a href="howWeWork.php">How We Work</a>
+                    </li>
+                    <li id="menu-item-151" class="menu-item" style>
+                        <a href="people.php">People</a>
+                    </li>
+                    <li id="menu-item-151" class="menu-item ">
+                        <a href="portfolio.php">Portfolio</a>
+                    </li>
+                    <!--
+                    <li id="menu-item-151" class="menu-item">
+                        <a href="insights.php">Insights</a>
+                    </li>
+                  -->
+                    <li id="menu-item-151" class="menu-item">
+                        <a href="contact.php">Contact</a>
+                    </li>
+                    <li><a class="hover-effects nav-toggle" href="#"><span></span></a></li>
+                </ul>
+            </nav>
+
+        </div>
+
+    </header>
+        <div class="toggle--mobile-menu">
+          <a class="hover-effects nav-toggle" href="#"><span></span></a>
+        </div>
+
+    <main>
+
+        <section class="intro">
+            <div class="container">
+                <div class="intro-text-block">
+                    <h1 class="wow fadeInDown introfontsize">We dare  <br>to invest early.</h1>
+                    <div class="wow fadeIn" data-wow-delay="250ms">
+                        <p>Together with local Angel Partners, we invest early in innovative companies in Sweden, Finland and the Baltics.</p>
+                    </div>
+                </div>
+
+                <div class="arrow-icon-wrapper wow fadeIn" data-wow-delay="450ms">
+                    <a class="arrow-icon" href="" data-control-scrollto=".scrollto">
+                        <svg class="arrowdown" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="51.3px" height="25.9px" viewBox="0 0 51.3 25.9" style="enable-background:new 0 0 51.3 25.9;" xml:space="preserve">
+                            <path d="M25.6,25.9c-0.3,0-0.5-0.1-0.7-0.3L0.3,1.7c-0.4-0.4-0.4-1,0-1.4c0.4-0.4,1-0.4,1.4,0l24,23.3l24-23.3c0.4-0.4,1-0.4,1.4,0
+                            c0.4,0.4,0.4,1,0,1.4L26.3,25.7C26.2,25.8,25.9,25.9,25.6,25.9z"/>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </section>
+
+
+        <section id="page--home" class="home-box-grid index-padding-intro scrollto">
+            <div class="sevenboxcontainer inner">
+
+                <div class="row row1">
+                    <div class="grid-item-left1 copy-box removepadding grid-item-5 wow fadeIn mint-box wow fadeIn">
+                      <a href="howWeWork.php" class="piclink">
+                      </a>
+                      <div class="indexpicoverlay">
+                          <h2 class="indeximagetext" >How We Invest</h2>
+                      </div>
+                      <img class="image img-fluid" style='height: 100%; width: 100%; object-fit: cover' src="images/yacht2.jpg" alt="">
+                    </div>
+
+                    <div class="grid-item-right1 copy-box removepadding grid-item-5 wow fadeIn mint-box wow fadeIn">
+                      <a href="portfolio.php" class="piclink">
+                      </a>
+                      <div class="indexpicoverlay2">
+                          <h2 class="indeximagetext textdown">Portfolio</h2>
+                      </div>
+                      <img class="image img-fluid" style='height: 100%; width: 100%; object-fit: cover' src="images/Karma_team.jpg" alt="">
+                    </div>
+                </div>
+
+
+
+                    <div class="row row2">
+                        <a href="https://medium.com/j12ventures" class="grid-item copy-box grid-item-5 wow fadeIn mint-box wow fadeIn indexpictext">
+                            <h2>Read Our Medium Articles</h2>
+                        </a>
+                        <a href="event.php" class="grid-item copy-box grid-item-5 wow fadeIn mint-box wow fadeIn reversi">
+                            <h2>Find Out About Our Events</h2>
+                        </a>
+
+                        <a href="recentNews.php" class="grid-item copy-box grid-item-5 wow fadeIn mint-box wow fadeIn">
+                            <h2>Recent News</h2>
+                        </a>
+                    </div>
+
+
+
+                    <div class="row row3">
+                        <div class="grid-item-left2 copy-box removepadding grid-item-5 wow fadeIn mint-box wow fadeIn indexpictext">
+                          <a href="contact.php" class="piclink">
+                          </a>
+                          <div class="indexpicoverlay">
+                              <h2 class="indeximagetext textdown" >Get In Touch</h2>
+                          </div>
+                          <img class="image img-fluid" style='height: 100%; width: 100%; object-fit: cover' src="images/AlexPhoto.jpg" alt="">
+                        </div>
+
+                        <div class="grid-item-right2 copy-box removepadding grid-item-5 wow fadeIn mint-box wow fadeIn">
+                          <a href="people.php" class="piclink">
+                          </a>
+                          <div class="indexpicoverlay2">
+                              <h2 class="indeximagetext">The J12 Platform</h2>
+                          </div>
+                          <img class="image img-fluid" style='height: 100%; width: 100%; object-fit: cover' src="images/NetworkPhoto.jpg" alt="">
+                        </div>
+                    </div>
+
+
+                </div>
+        </section>
+    </main>
+    <footer>
+        <div class="container">
+            <div class="footer-nav row">
+                
+                <a href="privacypolicy.php"><p class="footerbutton"><strong>Privacy Policy</strong></p></a>
+                
+                
+              <div class="disclaimertext">
+                <p style="font-size:10px;" class="copyright hide-mobile">&copy;J12 Ventures AB (and affiliated entities),
+                  DHS Venture Partners AB and Angel Partners are separate legal persons.
+                  Each retain the right to make their own independent investment decisions without
+                  any binding legal effects for the other parties or further obligating any of the
+                  parties towards any third party entities unless otherwise explicitly agreed upon.</p>
+              </div>
+                <ul class="footer-links">
+                    <li class="follow-us-link block-link">Get connected</li>
+                    <li>
+                        <a href="music.php" class="icon" title="J12's playlists">
+                          <img src="images/Spotify-icon-white.png" class="social-icon"
+                               onmouseover="this.src='images/Spotify-icon-color.png';"
+                               onmouseout="this.src='images/Spotify-icon-white.png';">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.linkedin.com/company/j12-ventures-ab/" class="icon" target="_blank" title="Find on Linkedin">
+                          <img src="images/LinkedIn-icon-white.png" class="social-icon"
+                               onmouseover="this.src='images/LinkedIn-icon-color.png';"
+                               onmouseout="this.src='images/LinkedIn-icon-white.png';">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://medium.com/j12ventures" class="icon" target="_blank" title="Follow on Medium">
+                          <img src="images/Medium-icon-white.png" class="social-icon"
+                               onmouseover="this.src='images/Medium-icon-color.png';"
+                               onmouseout="this.src='images/Medium-icon-white.png';">
+                        </a>
+                    </li>
+                </ul>
+                <p style="font-size:11px;" class="copyright show-mobile">&copy;J12 Ventures AB (and affiliated entities),
+                  DHS Venture Partners AB and Angel Partners are separate legal persons.
+                  Each retain the right to make their own independent investment decisions without
+                  any binding legal effects for the other parties or further obligating any of the
+                  parties towards any third party entities unless otherwise explicitly agreed upon.</p>
+            </div>
+        </div>
+    </footer>
+
+
+
+
+    <script type="text/javascript">
+
+
+    </script>
+    <script type='text/javascript' src='wordpressfile.js'></script>
+
+    <!-- Google Analytics -->
+    <!-- End Google Analytics -->
+</body>
+
+</html>
